a13fa3aaf313bb7647a6bb3e7ef804c083b0336f
Pranjali Basmatkar <basmatkarpranjali@gmail.com>
2018-06-20 23:28:41 +0530

    Update speechrecog.py

--- speechrecog.py
+++ speechrecog.py
@@ -22,7 +22,7 @@ def assistant(data):
     if "where is" in data:
         data = data.split(" ")
         location = data[2]
-        speak("Hold on Frank, I will show you where " + location + " is.")
+        speak("Hold on, I will show you where " + location + " is.")
         os.system("chromium-browser https://www.google.nl/maps/place/" + location + "/&amp;")    
 
 class Action(object):
